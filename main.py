78fe36e7b452d8e563dd72cda9ae1cc85553855d
ochoajuanm <juan.martin.ochoa.92@gmail.com>
2022-05-24 00:00:50 -0300

    Modificaciones en método de scraping, uso de bs4 en vez de pandas

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import logging
 from datetime import date, timedelta
+from bs4 import BeautifulSoup
+import re
 
 import pandas as pd
+
+import requests
+
 from decouple import config
 from sqlalchemy import create_engine
 
 logger = logging.getLogger("").getChild(__name__)
-
-leagues = ['COPA DE LA LIGA', 'PREMIER LEAGUE', 'SERIE A', 'LA LIGA']
 
 today = date.today()
 yesterday = today - timedelta(days = 1) 
@@ -21,7 +24,7 @@ db = config('POSTGRES_DATABASE')
 dbschema = config('POSTGRES_SCHEMA')
 url = f'postgresql://{user}:{passwd}@{host}:{port}/{db}'
 
-# MAKE THIS IMPORTANT CHANGE TO USE AUTOCOMMIT TRANSACTIONS WITH BIT.IO
+
 eng = create_engine(
     url,
     connect_args={
@@ -29,107 +32,68 @@ eng = create_engine(
     isolation_level="AUTOCOMMIT")
 
 
-def extract_data(league):
-    try:
-        df = pd.read_html('https://www.promiedos.com.ar/ayer', match=league)
-    except UnicodeDecodeError as e:
-        return None
-    if league == 'LA LIGA':
-        df = df[-1]
-        if df.iloc[1, 0] != 'LA LIGA':
-            return None
-    return df[0]
-
-
-def transform_data(df):
-    df_partidos = pd.DataFrame(columns=['equipo_local',
-                                        'equipo_visitante',
-                                        'goles_local',
-                                        'goles_visitante',
-                                        'torneo',
-                                        'goleadores_local',
-                                        'goleadores_visitante',
-                                        'estado_partido',
-                                        'detalle',
-                                        'fecha_partido'])
-    torneo = df.iloc[1, 0]
-    if df.iloc[2, 0] != 'Final':
-        detalle = df.iloc[2, 0]
-        arr = 1
-    else:
-        detalle = ''
-        arr = 0
-
-    estado_partido = 'Final'
-    indexes_to_drop = []
-
-    for i in range(len(df)):
-        if df.iloc[i, 2] == '0' and df.iloc[i, 3] == '0':
-            equipo_local = df.iloc[i, 1]
-            goles_local = df.iloc[i, 2]
-            goles_visitante = df.iloc[i, 3]
-            equipo_visitante = df.iloc[i, 4]
-            df_partidos = df_partidos.append(
-                {
-                    'equipo_local': equipo_local,
-                    'equipo_visitante': equipo_visitante,
-                    'goles_local': goles_local,
-                    'goles_visitante': goles_visitante,
-                    'torneo': torneo,
-                    'goleadores_local': '',
-                    'goleadores_visitante': '',
-                    'estado_partido': estado_partido,
-                    'detalle': detalle,
-                    'fecha_partido': yesterday
-                },
-                ignore_index=True)
-            indexes_to_drop = indexes_to_drop.append(i)
-
-    if indexes_to_drop is not None:
-        df.drop(index=indexes_to_drop, inplace=True)
-
-    # Fix out of range if len is less or equal to 3
-    if len(df) >= 4:
-        for i in range(2 + arr, len(df), 2):
-            equipo_local = df.iloc[i, 1]
-            goles_local = df.iloc[i, 2]
-            goles_visitante = df.iloc[i, 3]
-            equipo_visitante = df.iloc[i, 4]
-            goleadores_local = df.iloc[i + 1, 1]
-            goleadores_visitante = df.iloc[i + 1, 4]
-            df_partidos = df_partidos.append(
-                {
-                    'equipo_local': equipo_local,
-                    'equipo_visitante': equipo_visitante,
-                    'goles_local': goles_local,
-                    'goles_visitante': goles_visitante,
-                    'torneo': torneo,
-                    'goleadores_local': goleadores_local,
-                    'goleadores_visitante': goleadores_visitante,
-                    'estado_partido': estado_partido,
-                    'detalle': detalle,
-                    'fecha_partido': yesterday
-                },
-                ignore_index=True)
-
-    return df_partidos
-
-
-def load_data(df, eng, dbschema):
-    # Work with sqlalchemy as you normally would
-    with eng.connect() as conn:
-        df.to_sql(
-            'partidos',
-            schema=dbschema,
-            con=conn,
-            index=False,
-            if_exists='append')
-
+# def extract():
+#     r = requests.get('https://www.promiedos.com.ar/ayer')
+#     soup = BeautifulSoup(html_doc, 'html.parser')
 
 if __name__ == '__main__':
-    for league in leagues:
-        extract = extract_data(league)
-        if extract is None:
-            continue
-        transform = transform_data(extract)
-        load = load_data(transform, eng, dbschema)
+    page = requests.get('https://www.promiedos.com.ar/ayer')
+    soup = BeautifulSoup(page.text, 'html.parser')
+    tables = soup.find_all(id="fixturein")
+    ligas = []
+    fecha_partido = yesterday
+    df_columnas = ['equipo_local',
+                    'equipo_visitante',
+                    'goles_local',
+                    'goles_visitante',
+                    'torneo',
+                    'goleadores_local',
+                    'goleadores_visitante',
+                    'detalle',
+                    'fecha_partido']
+
+    df_partidos = pd.DataFrame(columns=df_columnas)
+
+    for table in tables:
+        torneo = table.find('tr', class_="tituloin").text.strip(' ')
+        partidos = table.find_all('tr', id=re.compile('^\d{1,3}_?\d+$'))
+        goles = table.find_all('tr', class_="goles")
+        try:
+            detalle = table.find('tr', class_="choy").text.strip(' ')
+        except Exception as e:
+            detalle = ''
+        for partido in partidos:
+            partido_id = partido.get('id')
+            equipo_local = partido.find('span', id=re.compile('^t1[\d_]*$')).text.strip(' ')
+            goles_local = partido.find('td', class_="game-r1").text.strip(' ')
+            goles_visitante = partido.find('td', class_="game-r2").text.strip(' ')
+            equipo_visitante = partido.find('span', id=re.compile('^t2[\d_]*$')).text.strip(' ')
+            try:
+                goleadores_local = partido.find('td', id="g1_"+partido_id).text.strip(' ')
+            except Exception as e:
+                goleadores_local = ''
+            try:
+                goleadores_visitante = partido.find('td', id="g2_"+partido_id).text.strip(' ')
+            except Exception as e:
+                goleadores_visitante = ''
+
+            df_partido = pd.DataFrame([[equipo_local,
+                                        equipo_visitante,
+                                        goles_local,
+                                        goles_visitante,
+                                        torneo,
+                                        goleadores_local,
+                                        goleadores_visitante,
+                                        detalle,
+                                        fecha_partido]], columns=df_columnas)
+            
+            df_partidos = pd.concat([df_partidos, df_partido])
+    # df_partidos.to_csv('output.csv', sep=';', encoding='utf8', index=False)
+
+    with eng.connect() as conn:
+        df_partidos.to_sql(
+                        'partidos',
+                        schema=dbschema,
+                        con=conn,
+                        index=False,
+                        if_exists='append')
