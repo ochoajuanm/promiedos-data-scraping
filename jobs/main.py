51f22bb9b954f98892238c07573f4c1a2eac65c0
ochoajuanm <juan.martin.ochoa.92@gmail.com>
2022-06-03 22:42:03 -0300

    test

--- jobs/main.py
+++ jobs/main.py
@@ -13,6 +13,7 @@ today = date.today()
 yesterday = today - timedelta(days=1)
 yesterday = yesterday.strftime("%d-%m-%Y")
 
+print('variables')
 user = config('POSTGRES_USER')
 passwd = config('POSTGRES_PASSWORD')
 host = config('POSTGRES_HOST')
@@ -20,23 +21,32 @@ port = config('POSTGRES_PORT')
 db = config('POSTGRES_DATABASE')
 dbschema = config('POSTGRES_SCHEMA')
 url = f'postgresql://{user}:{passwd}@{host}:{port}/{db}'
-
+print('variables conexion')
+print(user)
+print(passwd)
+print(host)
+print(port)
+print(db)
+print(dbschema)
+print(url)
 
 eng = create_engine(
     url,
     connect_args={
         'options': '-csearch_path={}'.format(dbschema)},
     isolation_level="AUTOCOMMIT")
-
+print('engine')
 
 def extract():
     page = requests.get('https://www.promiedos.com.ar/ayer')
     soup = BeautifulSoup(page.text, 'html.parser')
     tables = soup.find_all(id="fixturein")
+    print('fin de scraping')
     return tables
 
 
 def transform(tables):
+    print('inicio transform')
     fecha_partido = yesterday
     df_columnas = ['equipo_local',
                    'equipo_visitante',
@@ -88,11 +98,12 @@ def transform(tables):
                                         fecha_partido]], columns=df_columnas)
 
             df_partidos = pd.concat([df_partidos, df_partido])
-
+    print('Fin transform')
     return df_partidos
 
 
 def load(df):
+    print('inicio load')
     with eng.connect() as conn:
         df.to_sql(
             'partidos',
@@ -100,6 +111,7 @@ def load(df):
             con=conn,
             index=False,
             if_exists='append')
+    print('fin load')
 
 
 def main():
